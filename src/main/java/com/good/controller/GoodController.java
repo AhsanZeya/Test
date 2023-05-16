548ee59fbd515c7e0d252997e3a9efcf6488c620
AhsanZeya <ahsanzeya10@gmail.com>
2023-05-16 14:18:35 +0530

    First commit

---
+++ src/main/java/com/good/controller/GoodController.java
@@ -0,0 +1,14 @@
+package com.good.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GoodController {
+	
+	@RequestMapping("/")
+	public String newOk() {
+		return "new";
+	}
+
+}
